cc12c1b81de39aeba835b4d89b57e1d03fe181d1
Angelo Stewart <angelostewart@protonmail.com>
2023-02-28 15:26:41 -0500

    initial commit

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,21 @@
+import AnchorLink from "react-anchor-link-smooth-scroll"
+
+type Props = {
+    page: string;
+}
+
+const Link = (props: Props) => {
+    const lowerCasePage
+
+  return (
+   <AnchorLink
+   className={}
+   href={`home`}
+   onClick={}
+   >
+    {page}
+   </AnchorLink>
+  )
+}
+
+export default Link
